44e7d42cf0a775279048b7c3dc9a64db47dad1e6
sunhick@gmail.com <Sunhick@gmail.com>
2015-11-30 13:22:47 -0700

    fixed build issues

--- src/include/webproxy.h
+++ src/include/webproxy.h
@@ -33,22 +33,6 @@
 
 using namespace diagnostics;
 
-static const char *user_agent_str = "User-Agent: Mozilla/5.0 \
-(X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
-static const char *accept_str = "Accept: text/html,application/xhtml+xml,\
-application/xml;q=0.9,*/*;q=0.8\r\n";
-static const char *accept_encoding_str = "Accept-Encoding: \
-gzip, deflate\r\n";
-static const char *connection_str = "Connection: close\r\n";
-static const char *proxy_connection_str = "Proxy-Connection: close\r\n";
-static const char *http_version_str = "HTTP/1.0\r\n";
-/* client_bad_request_str used when host is invalid */
-static const char *client_bad_request_str = "HTTP/1.1 400 \
-Bad Request\r\nServer: Apache\r\nContent-Length: 140\r\nConnection: \
-close\r\nContent-Type: text/html\r\n\r\n<html><head></head><body><p>\
-This webpage is not available, because DNS lookup failed.</p><p>\
-Powered by Rui Hu and Gao Hao.</p></body></html>";
-
 namespace webkit {
 
   typedef struct {
@@ -127,33 +111,6 @@ namespace webkit {
     static void* consumeRequest(void* info);
     static void* dispatcher(void* arg);
 
-    void job(int newfd);
-    int forward_to_server(int fd, int *to_server_fd, char *cache_id,
-			  void *cache_content, unsigned int *cache_length);
-
-    int forward_to_client_from_cache(int to_client_fd, void *cache_content,
-				     unsigned int cache_length);
-    int forward_to_client(int to_client_fd, int to_server_fd);
-    /* int forward_to_client_from_cache(int to_client_fd, void *cache_content, */
-    /* 				     unsigned int cache_length); */
-    int forward_to_client_and_cache(int to_client_fd, int to_server_fd,
-				    char *cache_id, void *cache_content);
-    void close_fd(int *to_client_fd, int *to_server_fd);
-    void rio_readinitb(rio_t *rp, int fd);
-    ssize_t Rio_readlineb(rio_t *rp, void *usrbuf, size_t maxlen);
-    int parse_request_line(char *buf, char *method, char *protocol,
-				      char *host_port, char *resource, char *version);
-    void parse_host_port(char *host_port, char *remote_host, char *remote_port);
-    int open_clientfd(char *hostname, int port);
-    ssize_t rio_writen(int fd, void *usrbuf, size_t n);
-    ssize_t Rio_writen(int fd, void *usrbuf, size_t n);
-    ssize_t Rio_readnb(rio_t *rp, void *usrbuf, size_t n);
-    ssize_t rio_readnb(rio_t *rp, void *usrbuf, size_t n);
-    void get_size(char *buf, unsigned int *size_pointer);
-    ssize_t rio_readlineb(rio_t *rp, void *usrbuf, size_t maxlen);
-    ssize_t rio_read(rio_t *rp, char *usrbuf, size_t n);
-    int append_content(char *content, unsigned int *content_length,
-			      char *buf, unsigned int length);
   public:
     explicit web_proxy();
     virtual ~web_proxy();
